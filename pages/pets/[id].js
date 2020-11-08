fee7eb8d84d02818557fe105080b2fd0f655d15c
JP <joao.galindo@hotmail.com.br>
2020-11-08 15:25:08 -0300

    fix url in [id].js

--- pages/pets/[id].js
+++ pages/pets/[id].js
@@ -48,7 +48,7 @@ export default function PetId({ data }) {
 
 export async function getServerSideProps(context) {
 
-    const res = await fetch(`http://localhost:3000/api/pets/${context.query.id}`)
+    const res = await fetch(`https://next-mongodb-bootstrap.vercel.app/api/pets/${context.query.id}`)
     const data = await res.json()
 
     return {
